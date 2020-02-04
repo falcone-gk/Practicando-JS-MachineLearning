22aa4157518fc338bf95793db07f7ce8adcaa2f7
Enzo Falcon <falcone134@gmail.com>
2020-02-04 16:18:45 -0500

    covirtiendo kmeans_ex.js en una funcion

--- kmeans_ex.js
+++ kmeans_ex.js
@@ -1,64 +1,69 @@
 const kmeans = require('ml-kmeans');
 const plotly = require('nodeplotlib');
 const near = require('ml-nearest-vector');
-var DataFrame = require('dataframe-js').DataFrame;
-var linearAlgebra = require('linear-algebra')(),
+const DataFrame = require('dataframe-js').DataFrame;
+const linearAlgebra = require('linear-algebra')(),
     Matrix = linearAlgebra.Matrix;
 
-const iris = require('js-datasets-iris')
-const cols = ["sepalLength", "sepalWidth", "petalLength", "petalWidth", "specisies"]
+function show_model() {
 
-const df = new DataFrame(iris.data, cols)
-let y = df.select("specisies")
-let X = df.select("sepalWidth", "petalLength", "petalWidth")
-let m = new Matrix(X.toArray()).trans()
+    const iris = require('js-datasets-iris')
 
-let centers = [[2, 1, 0], [2, 1, 4], [3, 0, 2]]
-let ans = kmeans(X.toArray(), 3, { initialization: centers });
-let c1 = ans.centroids[0].centroid
-let c2 = ans.centroids[1].centroid
-let c3 = ans.centroids[2].centroid
-let m_centroids = new Matrix([c1, c2, c3])
-let cent_trans = m_centroids.trans()
+    const cols = ["sepalLength", "sepalWidth", "petalLength", "petalWidth", "specisies"]
 
-let g1 = new Array()
-let g2 = new Array()
-let g3 = new Array()
+    const df = new DataFrame(iris.data, cols)
+    let y = df.select("specisies")
+    let X = df.select("sepalWidth", "petalLength", "petalWidth")
 
-for (let i=0; i < X.dim()[0]; ++i) {
+    let centers = [[2, 1, 0], [2, 1, 4], [3, 0, 2]]
+    let ans = kmeans(X.toArray(), 3, { initialization: centers });
+    let c1 = ans.centroids[0].centroid
+    let c2 = ans.centroids[1].centroid
+    let c3 = ans.centroids[2].centroid
+    let m_centroids = new Matrix([c1, c2, c3])
+    let cent_trans = m_centroids.trans()
 
-    var vector = X.toArray()[i]
-    var c_nearest = near.findNearestVector(m_centroids.data, vector)
-    var ind = m_centroids.data.indexOf(c_nearest)
+    let g1 = new Array()
+    let g2 = new Array()
+    let g3 = new Array()
 
-    if (ind == 0) {
-        g1.push(vector)
-    } else if (ind == 1) {
-        g2.push(vector)
-    } else {
-        g3.push(vector)
+    for (let i=0; i < X.dim()[0]; ++i) {
+
+        var vector = X.toArray()[i]
+        var c_nearest = near.findNearestVector(m_centroids.data, vector)
+        var ind = m_centroids.data.indexOf(c_nearest)
+
+        if (ind == 0) {
+            g1.push(vector)
+        } else if (ind == 1) {
+            g2.push(vector)
+        } else {
+            g3.push(vector)
+        }
+    };
+
+    function show_scatter(arr) {
+        const scatter = {
+            x: arr.data[0],
+            y: arr.data[1],
+            z: arr.data[2],
+            mode: 'markers',
+            type: 'scatter3d'
+        }
+        return scatter
     }
-};
 
-function show_scatter(arr) {
-    const scatter = {
-        x: arr.data[0],
-        y: arr.data[1],
-        z: arr.data[2],
-        mode: 'markers',
-        type: 'scatter3d'
+    let gr1 = show_scatter(new Matrix(g1).trans())
+    let gr2 = show_scatter(new Matrix(g2).trans())
+    let gr3 = show_scatter(new Matrix(g3).trans())
+    let gr_c = show_scatter(cent_trans)
+
+    var layout = {
+        width: 1000,
+        height: 1000
     }
-    return scatter
+
+    plotly.plot([gr1, gr2, gr3, gr_c], layout)
 }
 
-let gr1 = show_scatter(new Matrix(g1).trans())
-let gr2 = show_scatter(new Matrix(g2).trans())
-let gr3 = show_scatter(new Matrix(g3).trans())
-let gr_c = show_scatter(cent_trans)
-
-var layout = {
-    width: 1000,
-    height: 1000
-}
-
-plotly.plot([gr1, gr2, gr3, gr_c], layout)
+show_model()
